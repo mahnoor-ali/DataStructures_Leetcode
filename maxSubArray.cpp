2f1c9583020bce1b29f77db23715e7c3467cfe64
Mahnoor <mahnoorali57se@gmail.com>
2022-12-31 16:18:54 +0500

    alternate solution of maximum subarray

--- maxSubArray.cpp
+++ maxSubArray.cpp
@@ -1,9 +1,12 @@
 // 53. Maximum Subarray
 
 //Time complexity: O(n)
+#include <algorithm>
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int startIndex, endIndex;
         int maxSum = nums[0], currSum = nums[0];
         for(int i=1; i< nums.size(); i++)
         {
@@ -12,10 +15,30 @@ public:
                 currSum+=nums[i];
             }
             else
+            {
                 currSum=nums[i];
+                startIndex=i;
+            }
             if(currSum>maxSum)
+            {
             maxSum=currSum;
+            endIndex=i;
+            }
+
         }
+        cout << "Max Sum: " << maxSum << " |  Start Index: " << startIndex << " End Index: " << endIndex << endl;
         return maxSum;
     }
 };
+
+//OR---
+
+int max_subarray_sum(int nums[], int n) {
+  int max_sum = nums[0];
+  int curr_sum = nums[0];
+  for (int i = 1; i < n; i++) {
+    curr_sum = max(nums[i], curr_sum + nums[i]);
+    max_sum = max(max_sum, curr_sum);
+  }
+  return max_sum;
+}
