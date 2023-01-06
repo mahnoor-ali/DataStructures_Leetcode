6eb51f5a705d03439dfdea1133c509e2c99fce3c
Mahnoor <mahnoorali57se@gmail.com>
2023-01-07 01:20:16 +0500

    Roman to Integer

--- Roman_to_Integer.cpp
+++ Roman_to_Integer.cpp
@@ -1,10 +1,25 @@
 //Related Topics: Hash Table, Strings
 
 
-
 class Solution {
 public:
     int romanToInt(string s) {
-        
+        int sum = symbols[s[s.size()-1]]; // adding last element as it will always be summed
+        unordered_map<char, int> symbols = {  { 'I' , 1 },
+                                              { 'V' , 5 },
+                                              { 'X' , 10 },
+                                              { 'L' , 50 },
+                                              { 'C' , 100 },
+                                              { 'D' , 500 },
+                                              { 'M' , 1000 } };
+        for(int i=0; i<s.size()-1; i++)        {
+            if(symbols[s[i]]<symbols[s[i+1]]){
+                sum -= symbols[s[i]];
+            }
+            else{
+                sum += symbols[s[i]];
+            }
+        }
+        return sum;
     }
 };
