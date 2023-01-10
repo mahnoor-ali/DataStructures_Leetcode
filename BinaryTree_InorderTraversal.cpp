b2bd0e18486ab72cf00678e5ec58560f1b572752
Mahnoor <mahnoorali57se@gmail.com>
2023-01-11 01:40:54 +0500

    Binary Tree Inorder Traversal using stack

---
+++ BinaryTree_InorderTraversal.cpp
@@ -0,0 +1,21 @@
+// stack implementation 
+//time complexity: O(n) , space complexity: O(n)
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        stack<TreeNode*> todo;
+        while (root || !todo.empty()) {
+            while (root) {
+                todo.push(root);
+                root = root -> left;
+            }
+            root = todo.top();
+            todo.pop();
+            nodes.push_back(root -> val);
+            root = root -> right;
+        }
+        return nodes;
+    }
+};
