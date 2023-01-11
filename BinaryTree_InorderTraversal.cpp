f77fafb99a3342c458792db6b4515d3483d97763
Mahnoor <mahnoorali57se@gmail.com>
2023-01-12 01:43:17 +0500

    Binary Tree Inorder Traversal using recursion

--- BinaryTree_InorderTraversal.cpp
+++ BinaryTree_InorderTraversal.cpp
@@ -19,3 +19,24 @@ public:
         return nodes;
     }
 };
+
+
+//recursive implementation
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        inorder(root, nodes);
+        return nodes;
+    }
+private:
+    void inorder(TreeNode* root, vector<int>& nodes) {
+        if (!root) {
+            return;
+        }
+        inorder(root -> left, nodes);
+        nodes.push_back(root -> val);
+        inorder(root -> right, nodes);
+    }
+};
