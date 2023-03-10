7e49fbc41462d48a8f50dd2c29cb31604aeb0cd0
Mahnoor <mahnoorali57se@gmail.com>
2023-03-11 00:29:58 +0500

    Product of Array Except Self

---
+++ Product of Array Except Self.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size(), prefix = 1, postfix = 1;
+        vector<int> ans(n,1); //initialize array cells with 1
+        for(int i = 0; i < n; i++) {
+            ans[i] *= prefix;
+            prefix *= nums[i];
+        }
+        postfix = 1;
+        for(int i = n-1; i >= 0; i--) {
+            ans[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return ans;
+    }
+};
